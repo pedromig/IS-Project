e88c3d1678ae95c0a7ea2b8488e2d17dbdd2bc4c
IronMan988 <rodrigoff988@gmail.com>
2022-10-17 16:13:00 +0100

    more exs

--- src/Assignment-2/src/main/java/mei/uc/is/Exs.java
+++ src/Assignment-2/src/main/java/mei/uc/is/Exs.java
@@ -2,11 +2,24 @@ package mei.uc.is;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
+import org.springframework.util.SocketUtils;
+
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 public class Exs {
     public static void main(String[] args) {
@@ -14,7 +27,11 @@ public class Exs {
 
         // assignment 2
         //ex1();
-        ex4();
+        try {
+            ex4();
+        } catch (Exception e) {
+
+        }
     }
 
     private static void ex1() {
@@ -48,28 +65,89 @@ public class Exs {
         System.out.println(numbers);
     }
 
-    private static void ex4() {
+    private static void ex4() throws InterruptedException, ExecutionException {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-        Flux.just(1, 2, 3, 4, 5)
+        //ex4
+        /*
+        Disposable flux = Flux.just(1, 2, 3, 4, 5)
             .log()
             .map(i -> i *2)
             .filter(v -> v > 5)
-            //.string(v: "Number " + v);
-            .subscribe(numbers::add);
+            .subscribe(x -> {System.out.println(x);});
 
-        Flux<String> flux = Flux.generate(
-            () -> 0,
-            (state, sink) -> {
-                sink.next("3 x " + state + " = " + 3*state);
-                if(state == 10) sink.complete();
-                return state + 1;
+        //ex5
+        Flux<String> flux5 = Flux.range(0, 10)
+            .map(n -> "Number " + n);
+
+        flux5.subscribe(x -> {System.out.println(x);});
+
+        //ex6
+        int window = 7, step = 1;
+        var flux6 = Flux.range(1, 100)
+            .buffer(window, step)
+            .skipLast(1)
+            .map(x -> x.stream(). reduce(Integer::sum).orElseThrow() / x.size())
+            .subscribe(x -> {System.out.println(x);});*/
+
+        //ex7
+        //var ex7 = Mono.from(Flux.range(0, 10)
+        //                    .collectList())
+        //        .subscribe(System.out::println);
+
+        //ex8
+        //var ex8 = Flux.repeat()
+
+        //class
+        ExecutorService es = Executors.newFixedThreadPool(4);
+
+        Set<Callable<String>> callableset = new HashSet<>();
+        callableset.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Task 1";
             }
-        );
+        });
 
-        System.out.println(flux.toString());
+        callableset.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Task 2";
+            }
+        });
 
-        //System.out.println(numbers);
+        callableset.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Task 3";
+            }
+        });
+
+        callableset.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Task 4";
+            }
+        });
+
+        callableset.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Task 5";
+            }
+        });
+
+        String res = es.invokeAny(callableset);
+        System.out.println(res);
+
+        List<Future<String>> res2 = es.invokeAll(callableset);
+        for(Future<String> r : res2)
+            System.out.println(r.get());
+
+        System.out.println("After all the gets");
+        es.shutdown();
+
+            
     }
 
     private static class MyComparator<T extends Integer> implements Comparator<T> {
