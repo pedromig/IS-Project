81280ff864cfbf6ad20f07af181c2116a66a3e95
Rodrigo Ferreira <76401919+IronFalcon0@users.noreply.github.com>
2022-11-10 22:38:59 +0000

    server logging implemented

--- src/Assignment-2/src/main/java/uc/mei/is/server/controller/StudentController.java
+++ src/Assignment-2/src/main/java/uc/mei/is/server/controller/StudentController.java
@@ -13,43 +13,53 @@ import uc.mei.is.server.entity.Student;
 import uc.mei.is.server.repository.RelationshipRepository;
 import uc.mei.is.server.repository.StudentRepository;
 
+import org.apache.log4j.Logger;
+
 @RestController
 @RequestMapping("/student")
 @RequiredArgsConstructor
 public class StudentController {
     private final StudentRepository studentRepository;
     private final RelationshipRepository relationshipRepository;
+    static Logger log = Logger.getLogger(StudentController.class.getName());
+
 
     @PostMapping
     public Mono<Student> create(@RequestBody Student student) {
+        log.info("Student created: " + student.toString());
         return this.studentRepository.save(student);
     }
 
     @PutMapping
     public Mono<Student> update(@RequestBody Student student) {
+        log.info("Student updated: " + student.toString());
         return this.studentRepository.findById(student.getId())
                                 .flatMap(x -> this.studentRepository.save(student));
     }
 
     @GetMapping(value = "/list")
     public Flux<Student> getAll() {
+        log.info("All students listed");
         return this.studentRepository.findAll();
     }
 
     @GetMapping(value = "/{id}")
     public Mono<ResponseEntity<Student>> get(@PathVariable int id) {
+        log.info("Student " + id + " listed");
         return this.studentRepository.findById(id)
                                 .map(ResponseEntity::ok)
                                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping(value = "/{id}")
-    public Mono<Void> delete(@PathVariable int id) {
-        return this.studentRepository.deleteById(id);
+    public Mono<Void> delete(@RequestBody Student student) {
+        log.info("Student " + student.getId() + " deleted");
+        return this.studentRepository.deleteById(student.getId());
     }
 
     @GetMapping(value = "/{id}/supervisors")
     public Flux<Integer> supervisorList(@PathVariable int id) {
+        log.info("Supervisors of student " + id + " listed");
         return this.relationshipRepository.findAllByStudentId(id).map(Relationship::getTeacherId);
     }
 }
